60628b36084b0c1df159406b669ae70faee0605f
Doniyor Mamutov <free22inc@gmail.com>
2024-07-15 23:19:45 +0500

    added test

---
+++ TestTask.Test/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using TestTask.Models;
+using Xunit;
+
+namespace TestTask.Test
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void TotalPriceWithVAT_ShouldCalculateCorrectly()
+        {
+            // Arrange
+            var product = new Product { Quantity = 10, Price = 50m };
+            var vatRate = 0.2m;
+
+            // Act
+            var result = product.TotalPriceWithVAT(vatRate);
+
+            // Assert
+            Assert.Equal(600m, result);
+        }
+
+    }
+}
